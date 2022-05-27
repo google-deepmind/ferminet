403932bc40330c597abad5b79fd61964d0d60cd0
paolo.chignoli <102721499+ChignoliPaolo@users.noreply.github.com>
2022-05-27 11:30:14 +0200

    Update train.py
    alpha_scale to control e-e interaction

--- ferminet/train.py
+++ ferminet/train.py
@@ -344,7 +344,7 @@ def train(molecule: Sequence[system.Atom],
           multi_gpu: bool = False,
           double_precision: bool = False,
           graph_path: Optional[str] = None,
-          alpha_scale = 0.,         
+          alpha_scale = 0.         
          ):
   
   """Configures and runs training loop.
@@ -439,7 +439,7 @@ def train(molecule: Sequence[system.Atom],
     if pretrain_config.iterations > 0:
       scf_approx.run()
 
-    hamiltonian_ops = hamiltonian.operators(molecule, nelectrons)
+    hamiltonian_ops = hamiltonian.operators(molecule, nelectrons,alpha_scale)
     if mcmc_config.init_means:
       if len(mcmc_config.init_means) != 3 * nelectrons:
         raise RuntimeError('Initial electron positions of incorrect shape. '
@@ -540,5 +540,4 @@ def train(molecule: Sequence[system.Atom],
       write_graph=os.path.abspath(graph_path) if graph_path else None,
       burn_in=mcmc_config.burn_in,
       mcmc_steps=mcmc_config.steps,
-      alpha_scale=alpha_scale,
       )
